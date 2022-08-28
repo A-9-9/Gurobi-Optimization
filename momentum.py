f863eefd4ce8ff00a5db0b0cb94371ab68319452
Ken <10846013@ntub.edu.tw>
2022-08-28 23:27:11 +0800

    add result in excel

--- momentum.py
+++ momentum.py
@@ -5,7 +5,7 @@ import copy
 
 # df usage: df.iloc[:, 2:]
 open_df = pd.read_excel('../result_1.xlsx', sheet_name='open').iloc[160:, :]
-return7comp_df = pd.read_excel('../result_1.xlsx', sheet_name='return7comp')
+return7comp_df = pd.read_excel('../result_1.xlsx', sheet_name='return7comp').iloc[:, :-3]
 
 comp_name = list(open_df.iloc[:, 2:].columns)
 
@@ -19,17 +19,17 @@ weight_selling_df = pd.DataFrame(columns=comp_name)
 weight_shortsell_df = pd.DataFrame(columns=comp_name)
 weight_repurchasing_df = pd.DataFrame(columns=comp_name)
 
-market_value_df = pd.DataFrame(columns=['Market value'])
+market_value_df = pd.DataFrame(
+    columns=['index', 'Date', 'Market value', 'out_obj', 'share+', 'share-', 'weight_ts', 'ts'])
 allocate_plus_df = pd.DataFrame(columns=comp_name)
 allocate_minus_df = pd.DataFrame(columns=comp_name)
 shares_plus_df = pd.DataFrame(columns=comp_name)
 shares_minus_df = pd.DataFrame(columns=comp_name)
 
 # for t
-for t in range(3):
+for t in range(93):
 
-# t = 0
-    date = open_df.iloc[t].loc["Date"]
+    # t = 0
     ret7comp = return7comp_df.iloc[t, 2:]
     # o = open_df.iloc[20*t, 2:]
 
@@ -37,19 +37,17 @@ for t in range(3):
     m = gp.Model('momentum')
     m.params.NonConvex = 2
 
-
     # rebal
     if t == 0:
         budget = 1000000
         weight_plus_previous = [0 for x in range(466)]
         weight_minus_previous = [0 for x in range(466)]
     else:
-        long_position = [x*y for x, y in zip(shares_plus_df.iloc[t - 1], open_df.iloc[20*t, 2:])]
-        short_position = [x*y for x, y in zip(shares_minus_df.iloc[t - 1], open_df.iloc[20*t, 2:])]
+        long_position = [x * y for x, y in zip(shares_plus_df.iloc[t - 1], open_df.iloc[20 * t, 2:])]
+        short_position = [x * y for x, y in zip(shares_minus_df.iloc[t - 1], open_df.iloc[20 * t, 2:])]
         budget = sum(long_position) + sum(short_position)
         weight_plus_previous = [x / budget for x in long_position]
         weight_minus_previous = [x / budget for x in short_position]
-
 
     # coefficient setting
     buy_position = {x: 0 for x in n}
@@ -79,7 +77,6 @@ for t in range(3):
     weight_shortsell = {}
     weight_repurchasing = {}
 
-
     for i in n:
         e_plus[i] = m.addVar(lb=0.0, ub=1.0, name="entropy-plus-%s" % i)
         e_minus[i] = m.addVar(lb=0.0, ub=1.0, name="entropy-minus-%s" % i)
@@ -95,7 +92,8 @@ for t in range(3):
     # objective setting
     entropy = quicksum(e_plus[i] + e_minus[i] for i in n)
     m_obj = -entropy - quicksum(
-        weight_buying[i] * ts1 + weight_selling[i] * ts2 + weight_shortsell[i] * ts3 + weight_repurchasing[i] * ts4 for i in
+        weight_buying[i] * ts1 + weight_selling[i] * ts2 + weight_shortsell[i] * ts3 + weight_repurchasing[i] * ts4 for
+        i in
         n)
     m.setObjective(m_obj, GRB.MAXIMIZE)
 
@@ -120,9 +118,27 @@ for t in range(3):
         m.addConstr(weight_minus[i] <= up * shortsell_position[i])
         m.addConstr(buy_position[i] + shortsell_position[i] <= 1)
     m.setParam('OutputFlag', 0)
+    transaction_cost = quicksum(weight_buying[i] * ts1 + weight_selling[i] * ts2 + weight_shortsell[
+        i] * ts3 + weight_repurchasing[i] * ts4 for i in n)
+
     m.optimize()
-# get the result after optimize
-# result write into dataframe
+
+    # print('m_obj %s' % m.getObjective().getValue())
+    ts_zip = zip([wb.x * ts1 for wb in weight_buying.values()], [v.x * ts2 for v in weight_selling.values()],
+                 [v.x * ts3 for v in weight_shortsell.values()], [v.x * ts4 for v in weight_repurchasing.values()])
+
+    date = open_df.iloc[20 * t].loc["Date"]
+    index = return7comp_df.iloc[t].loc["index"]
+    obj = m.getObjective().getValue()
+    share_plus = sum([1 for v in weight_plus.values() if v.x > 0])
+    share_minus = sum([1 for v in weight_minus.values() if v.x > 0])
+    weight_transaction_cost = sum([a + b + c + d for a, b, c, d in ts_zip])
+    ts = budget * weight_transaction_cost
+
+    result = [index, date, budget, obj, share_plus, share_minus, weight_transaction_cost, ts]
+    # print('ts_c %s' % transaction_cost)
+    # get the result after optimize
+    # result write into dataframe
     e_plus_df.loc[t] = [v.x for k, v in e_plus.items()]
     e_minus_df.loc[t] = [v.x for k, v in e_minus.items()]
     weight_df.loc[t] = [v.x for k, v in weight.items()]
@@ -133,18 +149,17 @@ for t in range(3):
     weight_shortsell_df.loc[t] = [v.x for k, v in weight_shortsell.items()]
     weight_repurchasing_df.loc[t] = [v.x for k, v in weight_repurchasing.items()]
 
-    market_value_df.loc[t] = [budget]
+    market_value_df.loc[t] = result
     allocate_plus_df.loc[t] = [budget * x for x in weight_plus_df.iloc[t]]
     allocate_minus_df.loc[t] = [budget * x for x in weight_minus_df.iloc[t]]
-    shares_plus_df.loc[t] = [x/y for x, y in zip(allocate_plus_df.iloc[t], open_df.iloc[20*t, 2:])]
-    shares_minus_df.loc[t] = [x/y for x, y in zip(allocate_minus_df.iloc[t], open_df.iloc[20*t, 2:])]
+    shares_plus_df.loc[t] = [x / y for x, y in zip(allocate_plus_df.iloc[t], open_df.iloc[20 * t, 2:])]
+    shares_minus_df.loc[t] = [x / y for x, y in zip(allocate_minus_df.iloc[t], open_df.iloc[20 * t, 2:])]
 
 # market_value_df = pd.DataFrame(columns=['Market value'])
 # allocate_plus_df = pd.DataFrame(columns=comp_name)
 # allocate_minus_df = pd.DataFrame(columns=comp_name)
 # shares_plus_df = pd.DataFrame(columns=comp_name)
 # shares_minus_df = pd.DataFrame(columns=comp_name)
-
 
 
 # df = pd.read_excel('../result_1.xlsx', sheet_name='return7comp')
@@ -156,7 +171,7 @@ for t in range(3):
 # w_plus_df.loc[len(w_plus_df.index)] = w_p
 # w_minus_df.loc[len(w_minus_df.index)] = w_m
 
-with pd.ExcelWriter('0827.xlsx') as writer:
+with pd.ExcelWriter('0828.xlsx') as writer:
     weight_df.to_excel(writer, sheet_name='weight')
     weight_plus_df.to_excel(writer, sheet_name='weight_plus')
     weight_minus_df.to_excel(writer, sheet_name='weight_minus')
@@ -167,7 +182,7 @@ with pd.ExcelWriter('0827.xlsx') as writer:
     weight_shortsell_df.to_excel(writer, sheet_name='weight_shortsell')
     weight_repurchasing_df.to_excel(writer, sheet_name='weight_repurchasing')
 
-    market_value_df.to_excel(writer, sheet_name='market_value')
+    market_value_df.to_excel(writer, sheet_name='result')
     allocate_plus_df.to_excel(writer, sheet_name='allocate_plus')
     allocate_minus_df.to_excel(writer, sheet_name='allocate_minus')
     shares_plus_df.to_excel(writer, sheet_name='shares_plus')
